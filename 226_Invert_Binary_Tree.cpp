63241e96a8c6f4142aeb20fe101930e4ff8bf78e
trungkodz <ntt2007qbhg@gmail.com>
2026-04-15 22:24:10 +0700

    226_Invert_Binary_Tree.cpp

---
+++ 226_Invert_Binary_Tree.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        
+        if(root == nullptr){
+            return nullptr;
+        }
+
+        TreeNode* leftinverttree = invertTree(root->left);
+        TreeNode* rightinverttree = invertTree(root->right);
+
+        root->left = rightinverttree;
+        root->right = leftinverttree;
+        
+        return root;
+       
+        }
+    };
+
+int main(){
+
+    return 0;
+}
